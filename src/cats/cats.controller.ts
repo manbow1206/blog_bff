2a3e9e596386de561f05e98ba8f9ba891a1d316f
TakuyaYasunaga <whaleshark5613@takuyayaganombp.lan>
2023-02-26 15:35:42 +0900

    add: dto

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,19 +1,33 @@
-import { Controller, Get, HttpCode, Param, Post, Req } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    HttpCode,
+    Param,
+    Post,
+    Put,
+    Query,
+    Req,
+} from '@nestjs/common';
+import { CreateCatDto, ListAllEntities, UpdateCatDto } from './dto/cat.dto';
 
 @Controller('cats')
 export class CatsController {
-    // @Post()
-    // create(): string {
-    //     return 'This actions adds a nes cat';
-    // }
+    @Post()
+    create(@Body() CreateCatDto: CreateCatDto): string {
+        return 'This actions adds a nes cat';
+    }
     @Get()
-    async findAll(@Req() request: Request): Promise<any[]> {
+    async findAll(@Query() query: ListAllEntities): Promise<any[]> {
         return [];
     }
-
     @Get(':id')
     findOne(@Param() params): string {
         console.log(params.id);
         return `This action returns a #${params.id} cat`;
+    }
+    @Put()
+    update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+        return ``;
     }
 }
