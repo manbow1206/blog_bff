cff8d3e661d075c3ed4577a5f44dfab1c9aacfdf
TakuyaYasunaga <whaleshark5613@takuyayaganombp.lan>
2023-02-26 16:04:20 +0900

    add:cats create method call service

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -10,13 +10,15 @@ import {
     Query,
     Req,
 } from '@nestjs/common';
+import { CatsService } from './cats.service';
 import { CreateCatDto, ListAllEntities, UpdateCatDto } from './dtos/cat.dto';
 
 @Controller('cats')
 export class CatsController {
+    constructor(private catsService: CatsService) {}
     @Post()
-    create(@Body() CreateCatDto: CreateCatDto): string {
-        return 'This actions adds a new cat';
+    create(@Body() createCatDto: CreateCatDto) {
+        return this.catsService.create(createCatDto);
     }
     @Get()
     async findAll(@Query() query: ListAllEntities) {
