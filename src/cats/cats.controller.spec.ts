3d6685a3bb61d7eaf2bba3199fac1733fe97cdaa
TakuyaYasunaga <whaleshark5613@takuyayaganombp.lan>
2023-02-26 17:10:35 +0900

    fix:unit test

--- src/cats/cats.controller.spec.ts
+++ src/cats/cats.controller.spec.ts
@@ -1,18 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
 
 describe('CatsController', () => {
   let controller: CatsController;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CatsController],
+      providers: [CatsService],
     }).compile();
 
     controller = module.get<CatsController>(CatsController);
   });
 
-  it.skip('should be defined', () => {
+  it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 });
