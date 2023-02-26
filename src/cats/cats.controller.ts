66e6cb0765ce4603b4454d2da96b84e5a008c929
TakuyaYasunaga <whaleshark5613@TakuyaYasunaganoMacBook-Pro.local>
2023-02-26 11:16:39 +0900

    add: traininng

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post, Req } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
+    // @Post()
+    // create(): string {
+    //     return 'This actions adds a nes cat';
+    // }
     @Get()
-    findAll(): string {
-        return '';
+    async findAll(@Req() request: Request): Promise<any[]> {
+        return [];
+    }
+
+    @Get(':id')
+    findOne(@Param() params): string {
+        console.log(params.id);
+        return `This action returns a #${params.id} cat`;
     }
 }
